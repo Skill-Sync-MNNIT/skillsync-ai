898d03603425c7dc15d31cf384fa46f5c7fb1dca
Yugank-563 <yugankkushwaha@gmail.com>
2026-04-16 16:47:44 +0530

    refactor(core): integrated all feature modules and optimized rate-limiting for chat

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './src/config/db.js';
 import { initFacultyScraperJob } from './src/jobs/facultyScraperJob.js';
 import { initJobExpiryJob } from './src/jobs/jobExpiry.js';
 import { initModerationWorker } from './src/jobs/moderation.processor.js';
+import { initSocket } from './src/services/socket.js';
 
 await connectDB();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  initSocket(server);
   initFacultyScraperJob();
   initJobExpiryJob();
   initModerationWorker();
